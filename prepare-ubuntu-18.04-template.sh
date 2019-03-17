fc0b008a7c4e9aa8359a5cd590cc58ed881a26d9
Philip Saad <philipsaad@users.noreply.github.com>
2019-03-17 11:57:09 -0700

    Updated for alternate installer, without cloud-init
    Personal fork for my own needs

--- prepare-ubuntu-18.04-template.sh
+++ prepare-ubuntu-18.04-template.sh
@@ -3,10 +3,11 @@
 #### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
 ######################################################
 # modified from: jcppkkk/prepare-ubuntu-template.sh
-# TESTED ON UBUNTU 18.04 LTS
+# TESTED ON UBUNTU 18.04 LTS (Alternate installer, without cloud-init)
+# http://cdimage.ubuntu.com/releases/18.04.2/release/ubuntu-18.04.2-server-amd64.iso
 
 # SETUP & RUN
-# curl -sL https://raw.githubusercontent.com/jimangel/ubuntu-18.04-scripts/master/prepare-ubuntu-18.04-template.sh | sudo -E bash -
+# curl -sL https://raw.githubusercontent.com/philipsaad/ubuntu-18.04-scripts/master/prepare-ubuntu-18.04-template.sh | sudo -E bash -
 
 if [ `id -u` -ne 0 ]; then
 	echo Need sudo
@@ -68,8 +69,6 @@ EOL
 chmod +x /etc/rc.local
 
 #reset hostname
-# prevent cloudconfig from preserving the original hostname
-sed -i 's/preserve_hostname: false/preserve_hostname: true/g' /etc/cloud/cloud.cfg
 truncate -s0 /etc/hostname
 hostnamectl set-hostname localhost
 
@@ -79,13 +78,6 @@ apt clean
 # disable swap
 sudo swapoff --all
 sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
-
-# set dhcp to use mac - this is a little bit of a hack but I need this to be placed under the active nic settings
-# also look in /etc/netplan for other config files
-sed -i 's/optional: true/dhcp-identifier: mac/g' /etc/netplan/50-cloud-init.yaml
-
-# cleans out all of the cloud-init cache / logs - this is mainly cleaning out networking info
-sudo cloud-init clean --logs
 
 #cleanup shell history
 cat /dev/null > ~/.bash_history && history -c
