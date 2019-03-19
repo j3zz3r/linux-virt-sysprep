5252a2665fd5f94ee04355bfd249ee7ac51768f7
Philip Saad <philipsaad@users.noreply.github.com>
2019-03-18 17:48:26 -0700

    Create prepare-alpine-linux-virt-3.9.2-template.sh

---
+++ prepare-alpine-linux-virt-3.9.2-template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+######################################################
+#### WARNING PIPING TO BASH IS STUPID: DO NOT USE THIS
+######################################################
+# modified from: jcppkkk/prepare-ubuntu-template.sh
+# Tested Alpine Version 3.9.2 Virtual
+# http://dl-cdn.alpinelinux.org/alpine/v3.9/releases/x86_64/alpine-virt-3.9.2-x86_64.iso
+
+# SETUP & RUN
+# curl -sL https://github.com/philipsaad/linux-virt-sysprep/master/prepare-alpine-linux-virt-3.9.2-template.sh | sudo -E bash -
